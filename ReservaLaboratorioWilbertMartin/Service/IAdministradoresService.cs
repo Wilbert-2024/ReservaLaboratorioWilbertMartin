91641b9e597a975ddbb92eb8d4a83e02fd0e1e39
CARLOS FRANCISCO JZR <CARLOS FRANCISCO JZR@INFINIX>
2025-11-25 13:52:53 -0600

    Terminado el servace

---
+++ ReservaLaboratorioWilbertMartin/Service/IAdministradoresService.cs
@@ -0,0 +1,11 @@
+﻿using ReservaLaboratorioWilbertMartin.Models;
+
+namespace ReservaLaboratorioWilbertMartin.Service
+{
+    public interface IAdministradoresService
+    {
+        Task<IEnumerable<ReservaLaboratorio>> ObtenerReservasPendientesAsync();
+        Task AprobarReservaAsync(int reservaId, int administradorId);
+        Task RechazarReservaAsync(int reservaId, int administradorId, string motivoRechazo);
+    }
+}
---
+++ ReservaLaboratorioWilbertMartin/Service/ILaboratoriosService.cs
@@ -0,0 +1,11 @@
+﻿using ReservaLaboratorioWilbertMartin.Models;
+
+namespace ReservaLaboratorioWilbertMartin.Services
+{
+    public interface ILaboratoriosService
+    {
+        Task<IEnumerable<Laboratorio>> ObtenerTodosAsync();
+        Task<Laboratorio?> ObtenerPorIdAsync(int id);
+        Task<bool> EstaDisponibleAsync(int laboratorioId, DateTime fecha, string horaReserva);
+    }
+}
