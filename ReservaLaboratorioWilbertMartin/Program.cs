8f1e29a48e4ee23e05193093067fd9d1c705124e
Wilbert-2024 <srmanhattan115@gmail.com>
2025-11-26 01:13:33 -0600

    Config de Jwt y Smtp Modelos Jwtsetting SmtpSetting, clase program ppsethingsjason

--- ReservaLaboratorioWilbertMartin/Program.cs
+++ ReservaLaboratorioWilbertMartin/Program.cs
@@ -1,4 +1,107 @@
+using MartinWilbert.MappingProfiles;
+using MartinWilbert.Models.Settings;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using ReservaLaboratorioWilbertMartin.Data;
+using ReservaLaboratorioWilbertMartin.Repository;
+using ReservaLaboratorioWilbertMartin.Services;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// =================== SERVICIOS ======================
+
+// Controladores con vistas
+builder.Services.AddControllersWithViews();
+
+builder.Services.Configure<JwtSettings>(builder.Configuration.GetSection("Jwt"));
+builder.Services.Configure<SmtpSettings>(builder.Configuration.GetSection("Smtp"));
+
+// AutoMapper
+builder.Services.AddAutoMapper(cfg => cfg.AddMaps(typeof(UserProfile).Assembly));
+
+// DbContext
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ConexionBD")));
+
+// Inyección de dependencias
+builder.Services.AddScoped<IAdministradoresService, AuthService>();
+builder.Services.AddScoped<ITokenService, TokenService>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IEmailService, EmailService>();
+
+// Sesiones y cookies
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(30);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+
+builder.Services.AddHttpContextAccessor();
+
+// Autenticación
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddCookie(options =>
+{
+    options.LoginPath = "/Auth/Login"; //Esto esta pendiente de revisar
+    options.AccessDeniedPath = "/Auth/AccessDenied";
+    options.Cookie.HttpOnly = true;
+    options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
+})
+.AddJwtBearer(options =>
+{
+    var jwtSettings = builder.Configuration.GetSection("Jwt").Get<JwtSettings>();
+
+    options.Events = new JwtBearerEvents
+    {
+        OnMessageReceived = context =>
+        {
+            if (context.Request.Cookies.ContainsKey("access_token"))
+                context.Token = context.Request.Cookies["access_token"];
+            return Task.CompletedTask;
+        },
+        OnChallenge = context =>
+        {
+            if (!context.Response.HasStarted)
+            {
+                context.Response.Redirect("/Auth/Login"); // Revisar estas paginas y sus respectivos controladores
+                context.HandleResponse();
+            }
+            return Task.CompletedTask;
+        },
+        OnForbidden = context =>
+        {
+            if (!context.Response.HasStarted)
+            {
+                context.Response.Redirect("/Auth/AccessDenied"); // Revisar estas paginas y sus respectivos controladores
+            }
+            return Task.CompletedTask;
+        }
+    };
+
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = jwtSettings?.Issuer,
+        ValidAudience = jwtSettings?.Audience,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings?.Key!))
+    };
+});
+
+
+
+
+
 
 // Add services to the container.
 builder.Services.AddRazorPages();
